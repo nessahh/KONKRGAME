89fd1dd5463aaac41bc9e9dd7630a7a7570694fa
nessahh <kahinaness7@gmail.com>
2025-03-04 18:53:24 +0100

    Premier commit

---
+++ build/CMakeFiles/KonkrGame.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kahina/KonkrGame/src/Game.cpp" "CMakeFiles/KonkrGame.dir/src/Game.cpp.o" "gcc" "CMakeFiles/KonkrGame.dir/src/Game.cpp.o.d"
+  "/home/kahina/KonkrGame/src/Map.cpp" "CMakeFiles/KonkrGame.dir/src/Map.cpp.o" "gcc" "CMakeFiles/KonkrGame.dir/src/Map.cpp.o.d"
+  "/home/kahina/KonkrGame/src/Renderer.cpp" "CMakeFiles/KonkrGame.dir/src/Renderer.cpp.o" "gcc" "CMakeFiles/KonkrGame.dir/src/Renderer.cpp.o.d"
+  "/home/kahina/KonkrGame/src/main.cpp" "CMakeFiles/KonkrGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/KonkrGame.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/KonkrGame.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KonkrGame.dir/src/Game.cpp.o"
+  "CMakeFiles/KonkrGame.dir/src/Game.cpp.o.d"
+  "CMakeFiles/KonkrGame.dir/src/Map.cpp.o"
+  "CMakeFiles/KonkrGame.dir/src/Map.cpp.o.d"
+  "CMakeFiles/KonkrGame.dir/src/Renderer.cpp.o"
+  "CMakeFiles/KonkrGame.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/KonkrGame.dir/src/main.cpp.o"
+  "CMakeFiles/KonkrGame.dir/src/main.cpp.o.d"
+  "KonkrGame"
+  "KonkrGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KonkrGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
